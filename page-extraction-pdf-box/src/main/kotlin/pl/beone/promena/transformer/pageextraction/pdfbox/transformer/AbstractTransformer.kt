ad548c81a3c7d97401c4de0f04864b4554dd7765
Bartosz Skotarek <bzskotarek@gmail.com>
2019-09-30 14:28:34 +0200

    [added] Extraction implementation

--- page-extraction-pdf-box/src/main/kotlin/pl/beone/promena/transformer/pageextraction/pdfbox/transformer/AbstractTransformer.kt
+++ page-extraction-pdf-box/src/main/kotlin/pl/beone/promena/transformer/pageextraction/pdfbox/transformer/AbstractTransformer.kt
@@ -1,11 +1,16 @@
 package pl.beone.promena.transformer.pageextraction.pdfbox.transformer
 
+import org.apache.pdfbox.io.MemoryUsageSetting
+import org.apache.pdfbox.multipdf.PDFMergerUtility
+import org.apache.pdfbox.pdmodel.PDDocument
+import org.apache.pdfbox.pdmodel.PDPage
 import pl.beone.promena.transformer.applicationmodel.mediatype.MediaType
 import pl.beone.promena.transformer.contract.data.DataDescriptor
 import pl.beone.promena.transformer.contract.data.TransformedDataDescriptor
 import pl.beone.promena.transformer.contract.data.singleTransformedDataDescriptor
 import pl.beone.promena.transformer.contract.model.Data
 import pl.beone.promena.transformer.contract.model.Parameters
+import pl.beone.promena.transformer.pageextraction.pdfbox.applicationmodel.getPages
 import java.io.OutputStream
 
 internal abstract class AbstractTransformer {
@@ -17,9 +22,24 @@ internal abstract class AbstractTransformer {
     fun transform(singleDataDescriptor: DataDescriptor.Single, targetMediaType: MediaType, parameters: Parameters): TransformedDataDescriptor.Single {
         val (data, mediaType, metadata) = singleDataDescriptor
 
+        process(data, parameters)
+
         return singleTransformedDataDescriptor(createData(), metadata)
     }
 
-    private fun process(data: Data, mediaType: MediaType): Data =
-        data
+    private fun process(data: Data, parameters: Parameters) {
+        val pages = parameters.getPages()
+
+        PDDocument.load(data.getInputStream()).use { document ->
+            mergePages(pages.map(document::getPage))
+        }
+    }
+
+    private fun mergePages(pages: List<PDPage>) {
+        PDFMergerUtility()
+            .apply { destinationStream = getOutputStream() }
+            .also { it.addSources(pages.map(PDPage::getContents)) }
+            .also { it.mergeDocuments(MemoryUsageSetting.setupMainMemoryOnly()) }
+    }
+
 }
