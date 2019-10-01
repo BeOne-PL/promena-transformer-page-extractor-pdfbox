fc84fb6d2c60a153f21aa7a2b15cf66088860112
Bartosz Skotarek <bzskotarek@gmail.com>
2019-10-01 13:03:59 +0200

    [added] Example function

--- page-extraction-pdf-box-example/src/main/kotlin/pl/beone/promena/transformer/pageextraction/pdfbox/example/PdfBoxPageExtractionTransformerExample.kt
+++ page-extraction-pdf-box-example/src/main/kotlin/pl/beone/promena/transformer/pageextraction/pdfbox/example/PdfBoxPageExtractionTransformerExample.kt
@@ -4,6 +4,12 @@ import pl.beone.promena.transformer.contract.transformation.Transformation
 import pl.beone.promena.transformer.pageextraction.pdfbox.applicationmodel.pdfBoxPageExtractionParameters
 import pl.beone.promena.transformer.pageextraction.pdfbox.applicationmodel.pdfBoxPageExtractionTransformation
 
+fun promena(): Transformation {
+    // Data: example.pdf
+
+    return pdfBoxPageExtractionTransformation(pdfBoxPageExtractionParameters(pages = listOf(1, 3, 66)))
+}
+
 fun `promena _ relaxed`(): Transformation {
     // Data: example.pdf
 
